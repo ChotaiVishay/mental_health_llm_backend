7e7891d203a1c7d26148ffd3cb4280d483dfdc62
Vishay Chotai <vishay.chotai@student.unimelb.edu.au>
2025-10-13 23:46:01 +1100

    hosting(vercel): adding baseUrl parsing abilities.

--- frontend/web/vite.config.ts
+++ frontend/web/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// Vite-only config (no `test:` block here)
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   build: { outDir: 'dist' }
 })
