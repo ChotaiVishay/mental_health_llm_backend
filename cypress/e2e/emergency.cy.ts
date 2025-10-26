417a6539dcd8c66bd1b9f435a8db42c31a9d49a6
Vishay Chotai <vishay.chotai@student.unimelb.edu.au>
2025-10-26 22:06:36 +1100

    MHS-288 testing(smoke test): cypress end to end tests, new folder addition

--- cypress/e2e/emergency.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('Emergency guardrails (UI presence)', () => {
-  it('shows crisis/helpline language somewhere', () => {
-    cy.visit('/')
-    cy.contains(/emergency|urgent help|lifeline|000|13 11 14/i)
-  })
-})
--- cypress/e2e/home.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('Home page smoke', () => {
-  it('loads and shows something meaningful', () => {
-    cy.visit('/')
-    cy.contains(/mental health|search|login|services/i)
-  })
-})
